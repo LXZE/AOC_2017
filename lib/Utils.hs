430ed51b05daf22dd4592ee28c1452dc5c72fbcd
Paul Lim <hayashi.azi1mov@outlook.com>
2024-01-07 03:45:30 +0700

    day 1

--- lib/Utils.hs
+++ lib/Utils.hs
@@ -2,8 +2,11 @@ module Utils where
 
 import Text.Printf ( printf )
 import System.IO (readFile)
+import Data.Maybe (fromJust)
 import System.Exit ( ExitCode(ExitFailure), exitWith )
 import Control.Exception (catch, SomeException)
+import Control.Monad (join)
+import System.Clock
 
 inputFile :: String -> String
 inputFile date = "src/" ++ date ++ "/input.txt"
@@ -18,17 +21,26 @@ getRawInput date = catch (readFile $ inputFile date) handler
     handler ex = printf "\n[ERR] src/%s/input.txt not found, exit with code 1\n" date
       >> exitWith (ExitFailure 1)
 
-assertSolution :: Eq a => Int -> ([String] -> IO (Maybe a)) -> [String] -> a -> IO ()
+assertSolution :: (Eq a, Show a) => Int -> ([String] -> IO (Maybe a)) -> [String] -> a -> IO ()
 assertSolution part fn input expect = do
-  print $ "Part " ++ show part
+  putStrLn $ "Part " ++ show part
+  start <- getTime Monotonic
   result <- fn input
-  print $ case result of
-    Nothing -> "No result"
-    Just result -> show $ expect == result
+  stop <- getTime Monotonic
+  case result of
+    Nothing -> printf "No result"
+    Just result -> printf "Expect %s | Result %s (%s)\n" (show expect) (show result) (show $ expect == result)
+  printf "Exec time: %d ns\n" (toNanoSecs $ diffTimeSpec stop start)
   putStrLn ""
 
-runSolution :: Int -> (a -> IO (Maybe Int)) -> a -> IO ()
+runSolution :: (Show b) => Int -> (a -> IO (Maybe b)) -> a -> IO ()
 runSolution part fn input = do
-  print $ "Part " ++ show part
-  fn input >>= print
+  putStrLn $ "Part " ++ show part
+  start <- getTime Monotonic
+  result <- fn input
+  stop <- getTime Monotonic
+  putStrLn $ case result of
+    Nothing -> "No result"
+    Just result -> show result
+  printf "Exec time: %d ns\n" (toNanoSecs $ diffTimeSpec stop start)
   putStrLn ""
--- src/01/Solution.hs
+++ src/01/Solution.hs
@@ -5,16 +5,29 @@ import Utils (
 
 import System.Environment (getArgs, getProgName)
 import Data.Foldable (for_)
+import Data.Char (digitToInt)
+
+dup_head :: Int -> String -> String
+dup_head size str = take (length str + size) (cycle str)
+
+pair :: [Int] -> [(Int, Int)]
+pair (x:xs) | length xs > 0 = [(x, xs !! 0)] ++ pair xs
+pair _ = []
+
+pair' :: [Int] -> [(Int, Int)]
+pair' (x:xs) = [(x, xs !! (x - 1))] ++ pair xs
+pair' _ = []
 
 part1 :: [String] -> IO (Maybe Int)
-part1 s = do
-  print s
-  return Nothing
+part1 [str] = do
+  let res = sum $ map fst $ filter (\(a, b) -> a == b) $ pair $ map digitToInt (dup_head 1 str)
+  return (Just res)
 
 part2 :: [String] -> IO (Maybe Int)
-part2 s = do
-  print s
-  return Nothing
+part2 [str] = do
+  let res = sum $ map fst $ filter (uncurry (==)) $ zip s (drop (length s `div` 2) s)
+  return (Just $ res * 2)
+  where s = map digitToInt str
 
 test :: [([String], Int)]
 test = [
@@ -22,6 +35,15 @@ test = [
     (["1111"], 4),
     (["1234"], 0),
     (["91212129"], 9)
+  ]
+
+test2 :: [([String], Int)]
+test2 = [
+    (["1212"], 6),
+    (["1221"], 0),
+    (["123425"], 4),
+    (["123123"], 12),
+    (["12131415"], 4)
   ]
 
 main :: IO ()
@@ -32,6 +54,7 @@ main = do
     then do
       for_ test $ \(input, expect) -> do
         assertSolution 1 part1 input expect
+      for_ test2 $ \(input, expect) -> do
         assertSolution 2 part2 input expect
     else do
       input <- getInput date
